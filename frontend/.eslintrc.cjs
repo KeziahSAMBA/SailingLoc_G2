163397e78e951989260e35d7d5dd43ebffaf994d
KeziahSAMBA <sambakeziah9@gmail.com>
2026-05-13 19:07:28 +0200

    fix: add JSX support and react plugin to frontend ESLint config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,10 +3,19 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react"],
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
   rules: {},
 };
