1450f00759c3ddb09d93c1deb05d1dedc19a9105
KeziahSAMBA <sambakeziah9@gmail.com>
2026-05-13 19:03:55 +0200

    fix: rename eslintrc to .cjs and remove quotes from lint script for Windows compatibility

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,23 +1,12 @@
 module.exports = {
   env: {
     browser: true,
-    es2023: true,
+    es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  extends: ["eslint:recommended"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-  },
+  rules: {},
 };
