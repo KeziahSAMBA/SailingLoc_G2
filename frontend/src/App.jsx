8e0972ea8a45c1abe799325cba1c8f9678c9ee93
KeziahSAMBA <sambakeziah9@gmail.com>
2026-05-14 02:35:40 +0200

    feat(header): création header non connecté ~format desktop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import AppRouter from './router/AppRouter.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
+import Header from './components/common/Header/Header.jsx';
 
 function App() {
   return (
     <AuthProvider>
+      <Header />
       <div className="min-h-screen bg-slate-50 text-slate-900">
         <AppRouter />
       </div>
