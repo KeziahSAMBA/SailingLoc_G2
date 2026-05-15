61201b65c15a696957fa316e360c6b3138c4bfe7
KeziahSAMBA <sambakeziah9@gmail.com>
2026-05-15 20:40:11 +0200

    feat(header): création header-locataire

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import AppRouter from './router/AppRouter.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
-// import Header from './components/common/Header/Header.jsx';
-import HeaderLocataire from './components/common/Header/HeaderLocataire.jsx';
+import Header from './components/common/Header/Header.jsx';
 
 function App() {
   return (
     <AuthProvider>
-      {/* <Header /> */}
-      <HeaderLocataire />
+      <Header />
       <div className="min-h-screen bg-slate-50 text-slate-900">
         <AppRouter />
       </div>
