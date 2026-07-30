7a475c0580f85adbdf8953da8b045d970448eb7a
Lubi2003 <lucaspub2003@gmail.com>
2026-07-30 23:40:32 +0200

    fix: afficher le fichier sélectionné sur mobile

--- frontend/src/components/locataire/LocataireDocuments.jsx
+++ frontend/src/components/locataire/LocataireDocuments.jsx
@@ -24,7 +24,7 @@ function LocataireDocuments() {
         </p>
       </header>
 
-      <DocumentsManager onCounts={onCounts} />
+      <DocumentsManager onCounts={onCounts} stackFilePickerOnMobile />
     </section>
   );
 }
